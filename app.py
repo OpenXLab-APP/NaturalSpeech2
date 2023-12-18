173e528c32da241d90d740bf21e1e1fe3efa5040
Hecheng0625 <Hecheng0625@users.noreply.huggingface.co>
2023-12-18 07:31:29 +0000

    Update app.py

--- app.py
+++ app.py
@@ -46,6 +46,7 @@ def ns2_inference(
         nltk.download('cmudict')
     except:
         pass
+
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     os.environ["WORK_DIR"] = "./"
